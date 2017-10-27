39ef5860fff9ae1e645f152806febc83dd4f3139
Konofeev Roman <roman@konofeev.ru>
2017-10-27 16:54:28 +0300

    Temp key listener

--- src/main/java/ru/konofeev/gui/CommandLine.java
+++ src/main/java/ru/konofeev/gui/CommandLine.java
@@ -1,9 +1,6 @@
 package ru.konofeev.gui;
 
-import javafx.event.EventHandler;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 
 /**
  * Главное окно приложения
@@ -16,26 +13,5 @@ public class CommandLine extends TextField
     public CommandLine()
     {
 		super("Command Line");
-		keyPressed();
     }
-
-	private void keyPressed()
-	{
-		setOnKeyPressed
-		(
-			new EventHandler<KeyEvent>()
-			{
-				@Override
-				public void handle(KeyEvent keyEvent)
-				{
-					if (keyEvent.getCode().equals(KeyCode.ENTER))
-					{
-						System.out.println("enter");
-					}
-					System.out.println("key event");
-					setPressed(false);
-				}
-			}
-		);
-	}
 }
--- src/main/java/ru/konofeev/gui/Editor.java
+++ src/main/java/ru/konofeev/gui/Editor.java
@@ -1,17 +1,55 @@
 package ru.konofeev.gui;
 
 import javafx.scene.web.HTMLEditor;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 /**
  * Главное окно приложения
  */
 public class Editor extends HTMLEditor
 {
+	private String content;
     /**
      * Конструктор
      */
     public Editor()
     {
         setHtmlText("Html text");
+		keyPressed();
     }
+
+	private void keyPressed()
+	{
+		// Управление
+		setOnKeyPressed
+		(
+			new EventHandler<KeyEvent>()
+			{
+				@Override
+				public void handle(KeyEvent keyEvent)
+				{
+					//if (keyEvent.getCode().equals(KeyCode.ENTER))
+					System.out.println("setOnKeyPressed");
+					content = getHtmlText();
+				}
+			}
+		);
+
+		setOnKeyReleased
+		(
+			new EventHandler<KeyEvent>()
+			{
+				@Override
+				public void handle(KeyEvent keyEvent)
+				{
+					//if (keyEvent.getCode().equals(KeyCode.ENTER))
+					System.out.println("setOnKeyReleased");
+					setHtmlText(content);
+				}
+			}
+		);
+
+	}
 }
